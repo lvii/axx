3c576d71ee1c65b2857369290ce2d6e9925f7112
MuaCat <87961796+MuaCat@users.noreply.github.com>
2021-08-09 19:05:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #====================================================================
-# Copyright (c) 2019-2020 iplcdn <https://iplcdn.com>
+# Copyright (c) 2019-2021 iplcdn <https://iplcdn.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/insChow/Actions-OpenWrt
+# https://github.com/MuaCat/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #====================================================================

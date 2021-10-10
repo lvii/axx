b874ba418e4e4b9ddb015e4ed8b7138d44b16085
MuaCat <87961796+MuaCat@users.noreply.github.com>
2021-10-10 16:44:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,4 +25,3 @@
 #rm -rf package/lean/luci-theme-argon
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 #====================================================================
-sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default

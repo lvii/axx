f656cd2e2b6238f69b5c26e625f39b825039af29
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-03-11 23:14:59 +0800

    ip 192.168.1.1

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 #====================================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 #sed -i 's/OpenWrt/OpenWrt/g' package/base-files/files/bin/config_generate

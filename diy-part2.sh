baab0381afa137bd3a89c6c62486bb37acc8c314
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-04-08 18:26:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -70,7 +70,7 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-th
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
-git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
+#git clone https://github.com/kiddin9/openwrt-packages.git package/openwrt-packages
 
 git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 
@@ -83,8 +83,13 @@ git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-ipt
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 #添加smartdns
 git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
+
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# Add luci-theme-argon
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+#rm -rf ../lean/luci-theme-argon
 #================================================================================================
 git clone https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git
 #移除不用软件包    

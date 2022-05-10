6485a71043e00ea96d74e9b20c7a4347dfba5828
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-05-11 01:10:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -80,8 +80,9 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' packag
 #    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
 #================================================================================================
 git clone https://github.com/Boos4721/OpenWrt-Packages.git package/Boos4721-packages
-git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8-packages
+git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9-packages
 
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8-packages
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
 git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial

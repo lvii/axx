54c5182b70338adf93d7b246d00c6f2daa404b14
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-05-16 05:45:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -105,8 +105,8 @@ git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-
 #添加argon-config 使用 最新argon
 rm -rf package/lean/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
 
 #取消原主题luci-theme-bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap

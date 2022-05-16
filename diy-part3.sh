cf8fefa4c2e1887d6962d3b798f26b92dfa7d4ea
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-05-16 16:54:22 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -21,19 +21,18 @@ mv diy/bg/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luc
 mv diy/bg/logo.png openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/logo.png
 #mv diy/bg/bg1.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg1.jpg
 
-mv diy/bg-argon/bg1.jpg openwrt/package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+mv diy/bg-argon/bg1.jpg openwrt/package/kenzo/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 mv diy/bg-argon/bg1.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg1.jpg
 
 mv diy/bg/bg2.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg2.jpg
 mv diy/bg/bg3.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg3.jpg
 #------------------------------------------------------------
-mv diy/favicon.ico openwrt/package/kenzo/luci-theme-ifit/files/htdocs/favicon.ico
-mv diy/logo.png openwrt/package/kenzo/luci-theme-ifit/files/htdocs/logo.png      
-mv diy/bg/main_bg.jpg openwrt/package/kenzo/luci-theme-ifit/files/htdocs/images/main_bg.jpg
+mv diy/favicon.ico openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/favicon.ico
+mv diy/logo.png openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/logo.png       
+mv diy/bg/main_bg.jpg openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/images/main_bg.jpg
 
 #mv diy/bg/main_bg_5k.jpg openwrt/package/kenzo/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg
-mv diy/bg-argon/main_bg_5k.jpg openwrt/package/kenzo/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg
-
+mv diy/bg-argon/main_bg_5k.jpg openwrt/feeds/kenzo/luci-theme-ifit/files/htdocs/images/main_bg_5k.jpg
 mv diy/banner openwrt/package/base-files/files/etc/banner
 #------------------------------------------------------------
-#openwrt/package/kenzo/luci-theme-ifit/
+#openwrt/feeds/kenzo/luci-theme-ifit

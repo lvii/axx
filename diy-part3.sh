b3f85d5f8b03170179f2e9fb20f6c69087cca540
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-05-21 01:41:25 +0800

    Add files via upload

--- diy-part3.sh
+++ diy-part3.sh
@@ -18,13 +18,13 @@
 #favicon为128*128
 #------------------------------------------------------------------------------------------------------------------------
 mv diy/bg/favicon.ico openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/favicon.ico
-mv diy/bg/logo.png oopenwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/logo.png
+mv diy/bg/logo.png openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/logo.png
 mv diy/bg/bg1.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg1.jpg
 mv diy/bg/bg2.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg2.jpg
 mv diy/bg/bg3.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg3.jpg
 #------------------------------------------------------------------------------------------------------------------------
 mv diy/bg/bg1.jpg openwrt/package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-mv diy/bg/bg1.jpg openwrt/package/feeds/kenzo/luci-theme-argonne/htdocs/luci-static/argonne/img
+mv diy/bg/bg1.jpg openwrt/package/kenzok8-packages/luci-theme-argonne/htdocs/luci-static/argonne/img
 #------------------------------------------------------------------------------------------------------------------------
 mv diy/favicon.ico openwrt/package/lean/luci-theme-ifit/files/htdocs/favicon.ico
 mv diy/logo.png openwrt/package/lean/luci-theme-ifit/files/htdocs/logo.png      

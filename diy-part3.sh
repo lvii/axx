6f6b070081e6642058721fb4eeb02906f5af18fc
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-05-21 23:31:15 +0800

    Add files via upload

--- diy-part3.sh
+++ diy-part3.sh
@@ -23,7 +23,8 @@ mv diy/bg/bg1.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-st
 mv diy/bg/bg2.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg2.jpg
 mv diy/bg/bg3.jpg openwrt/package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/img/bg3.jpg
 #------------------------------------------------------------------------------------------------------------------------
-mv diy/bg/bg1.jpg openwrt/package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp diy/bg/bg1.jpg openwrt/package/lean/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp diy/bg/bg1.jpg openwrt/package/feeds/kenzo/luci-theme-argonne/htdocs/luci-static/argonne/img/bg1.jpg
 #------------------------------------------------------------------------------------------------------------------------
 mv diy/favicon.ico openwrt/package/feeds/kenzo/luci-theme-ifit/files/htdocs/favicon.ico
 mv diy/logo.png openwrt/package/feeds/kenzo/luci-theme-ifit/files/htdocs/logo.png      

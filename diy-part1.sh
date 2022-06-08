90efd9a27791be19afbfcce64e763fac2c1e7959
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-06-09 02:33:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,6 @@
 #====================================================================
 # 添加插件源码
 sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# nss_packages
-sed -i '$a src-git-full nss_packages https://github.com/robimarko/nss-packages' feeds.conf.default
 # passwall依赖
 sed -i '$a src-git-full small https://github.com/kenzok8/small' feeds.conf.default
 #====================================================================

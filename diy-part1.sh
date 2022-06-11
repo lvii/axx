e05eb6b998fcb085ec7f389bd7a40e7f4c515ceb
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-06-12 02:27:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,6 +21,8 @@
 #====================================================================
 # 添加插件源码
 sed -i '$a src-git-full kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# Argon_dark_purple
+sed -i '$a src-git-full liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 # passwall依赖
 sed -i '$a src-git-full small https://github.com/kenzok8/small' feeds.conf.default
 #====================================================================

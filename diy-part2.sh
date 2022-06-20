8e98e8f615de7f37e72bbe60472f5f4b7b465238
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-06-21 00:45:52 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -76,13 +76,92 @@ rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-theme-bootstrap
 rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
-rm -rf feeds/kenzo/luci-theme-argon
 
 #移除不用软件包    
 rm -rf feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/luci/applications/luci-app-wrtbwmon
 rm -rf feeds/packages/net/smartdns
 
+
+# themes
+#git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+#git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
+
+git clone https://github.com/Joecaicai/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
+
+#添加argon-config 使用最新argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
+
+#取消原主题luci-theme-bootstrap为默认主题
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# themes添加（svn co 命令意思：指定版本如https://github）
+#-------------------------------------------------------------------------------------------------------------------------------
+# 修改luci-theme-argon_armygreen主题渐变色，16进制RGB
+#登录页面背景颜色+半透明
+#sed -i 's/#f7fafc/rgba(134,176,197, .5)/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+
+#渐变色开始
+#sed -i 's/#f9ffff/#80ABC3/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#渐变色结束   
+#sed -i 's/#7fffffb8/#C3DEF1b8/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#sed -i 's/#9effff57/#9FC4D557/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+
+#应用+保存
+#sed -i 's/#46b8da/#407994/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#未选中
+#sed -i 's/#a2a287/#6F7E92/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#主机名  ，导航栏-状态-文字的颜色
+sed -i 's/#4fc352/#B7E0F3/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-系统-文字的颜色
+#sed -i 's/#fb6340/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-服务-文字的颜色
+#sed -i 's/#11cdef/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-网络存储-文字的颜色
+#sed -i 's/#f3a4b5/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-控制-文字的颜色
+#sed -i 's/#f3a4b5/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-VPN-文字的颜色
+#sed -i 's/#172b4d/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-网络-文字的颜色
+#sed -i 's/#8965e0/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-带宽监视器-文字的颜色
+#sed -i 's/#5e72e4/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-注销-文字的颜色
+#sed -i 's/#32325d/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+
+#导航栏-选中前的颜色
+#sed -i 's/#f6f9fc/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#导航栏-选中后的颜色
+#sed -i 's/#4fc352/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+
+#面板标题，如：导航栏-总览-系统 的颜色
+#sed -i 's/#32325d/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#面板标题，如：导航栏-总览-系统 触摸后按钮样式的颜色
+#sed -i 's/#F0F0F0/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#复位 背景 - 边框 按钮样式的颜色
+#sed -i 's/#f0ad4e/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#sed -i 's/#eea236/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#删除 背景 - 边框 按钮样式的颜色
+#sed -i 's/#fb6340/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+
+#登陆页面右下角技术支持  跳转网站“https://github.com/openwrt/luci”   名称“可爱动漫主题”
+#sed -i 's/可爱动漫主题/可爱动漫主题/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/footer.htm
+#主机名右上角符号❤
+#sed -i 's/❤/☯/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/header.htm
+
+#已选中
+#sed -i 's/#4F2EDC/#E0DAD6/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+
+#sed -i 's/#00FF00/#407994/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#加载背景
+#sed -i 's/#5e72e4/#407994/g' package/lean/luci-theme-argon_armygreen/htdocs/luci-static/argon_armygreen/css/style.css
+#-------------------------------------------------------------------------------------------------------------------------------
 #添加额外软件包
 # nss_packages
 #git clone https://github.com/robimarko/nss-packages package/nss_packages
@@ -103,83 +182,6 @@ find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_U
 
 #replace coremark.sh with the new one
 cp -f $GITHUB_WORKSPACE/general/coremark.sh feeds/packages/utils/coremark/
-
-# themes
-#git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
-#git clone https://github.com/YL2209/luci-theme-ifit.git package/lean/luci-theme-ifit
-
-#添加argon-config 使用最新argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon
-
-#取消原主题luci-theme-bootstrap为默认主题
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-# themes添加（svn co 命令意思：指定版本如https://github）
-#-------------------------------------------------------------------------------------------------------------------------------
-# 修改luci-theme-argon_armygreen主题渐变色，16进制RGB
-#登录页面背景颜色+半透明
-#sed -i 's/#f7fafc/rgba(134,176,197, .5)/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-
-#渐变色开始
-#sed -i 's/#f9ffff/#80ABC3/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#渐变色结束   
-#sed -i 's/#7fffffb8/#C3DEF1b8/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#sed -i 's/#9effff57/#9FC4D557/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-
-#应用+保存
-#sed -i 's/#46b8da/#407994/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#未选中
-#sed -i 's/#a2a287/#6F7E92/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#主机名  ，导航栏-状态-文字的颜色
-sed -i 's/#4fc352/#B7E0F3/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-系统-文字的颜色
-#sed -i 's/#fb6340/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-服务-文字的颜色
-#sed -i 's/#11cdef/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-网络存储-文字的颜色
-#sed -i 's/#f3a4b5/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-控制-文字的颜色
-#sed -i 's/#f3a4b5/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-VPN-文字的颜色
-#sed -i 's/#172b4d/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-网络-文字的颜色
-#sed -i 's/#8965e0/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-带宽监视器-文字的颜色
-#sed -i 's/#5e72e4/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-注销-文字的颜色
-#sed -i 's/#32325d/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-
-#导航栏-选中前的颜色
-#sed -i 's/#f6f9fc/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#导航栏-选中后的颜色
-#sed -i 's/#4fc352/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-
-#面板标题，如：导航栏-总览-系统 的颜色
-#sed -i 's/#32325d/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#面板标题，如：导航栏-总览-系统 触摸后按钮样式的颜色
-#sed -i 's/#F0F0F0/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#复位 背景 - 边框 按钮样式的颜色
-#sed -i 's/#f0ad4e/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#sed -i 's/#eea236/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#删除 背景 - 边框 按钮样式的颜色
-#sed -i 's/#fb6340/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-
-#登陆页面右下角技术支持  跳转网站“https://github.com/openwrt/luci”   名称“可爱动漫主题”
-#sed -i 's/可爱动漫主题/可爱动漫主题/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/footer.htm
-#主机名右上角符号❤
-#sed -i 's/❤/☯/g' package/lean/luci-theme-argon_armygreen/luasrc/view/themes/argon_armygreen/header.htm
-
-#已选中
-#sed -i 's/#4F2EDC/#E0DAD6/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-
-#sed -i 's/#00FF00/#407994/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
-#加载背景
-#sed -i 's/#5e72e4/#407994/g' openwrt/feeds/liuran001/luci-theme-argon-lr/htdocs/luci-static/argon_armygreen/css/style.css
 #-------------------------------------------------------------------------------------------------------------------------------
 ./scripts/feeds update -a
 ./scripts/feeds install -a

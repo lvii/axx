bb478e09fcba769d739798d0a7bfd682182db16c
Joecaicai <87961796+Joecaicai@users.noreply.github.com>
2022-06-24 03:12:08 +0800

    wifi

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,15 +36,22 @@ sed -i 's/OpenWrt /AutoBuild $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g' package/
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 
-# 修改默认wifi名称ssid为100
-#sed -i 's/ssid=OpenWrt/ssid=100/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
 #开启MU-MIMO
 #sed -i 's/mu_beamformer=0/mu_beamformer=1/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
+# 修改默认wifi名称ssid为100/101
+#sed -i 's/ssid=OpenWrt/ssid=100/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#5g
+sed -i '/	set wireless.default_radio0${devidx}.ssid=OpenWrt/a\set wireless.default_radio0${devidx}.ssid=101' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#2.4g
+sed -i '/ set wireless.default_radio1${devidx}.ssid=OpenWrt/a\set wireless.default_radio1${devidx}.ssid=100' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 #wifi加密方式encryption=sae-mixed
 #使用sed 在第四行后添加新字(wifi加密方式，没有是none,wifi密码key为gds.2021)
-sed -i '/set wireless.default_radio${devidx}.encryption=sae-mixed/a\set wireless.default_radio${devidx}.key=gds.2021' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#2.4g
+sed -i '/set wireless.default_radio0${devidx}.encryption=sae-mixed/a\set wireless.default_radio0${devidx}.key=gds.2021' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#5g
+sed -i '/set wireless.default_radio1${devidx}.encryption=sae-mixed/a\set wireless.default_radio1${devidx}.key=gds.2021' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #-------------------------------------------------------------------------------------------------------------------------------
 # 状态系统增加个性信息
 #sed -i "s/exit 0//" package/lean/default-settings/files/zzz-default-settings
